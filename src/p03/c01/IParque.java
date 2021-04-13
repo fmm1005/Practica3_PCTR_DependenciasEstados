1adeed75f6e471fa091de89849f4a2280de4394b
Francisco <fmm1005@alu.ubu.es>
2021-04-13 21:30:31 +0200

    Clases main lanzador y la interfaz de parque

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -1,9 +1,14 @@
 package src.p03.c01;
-
+/**
+ * Clase ActividadSalidaPuerta.
+ *
+ * @author Alvaro L�pez Pereda
+ * @author Francisco Medel Molinero
+ */
 public interface IParque {
 	
 	public abstract void entrarAlParque(String puerta);
 
-	// TODO
+	public abstract void salirDelParque(String puerta);
 
 }
