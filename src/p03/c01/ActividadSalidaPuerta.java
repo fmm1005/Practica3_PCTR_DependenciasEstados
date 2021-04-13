72ec51de66e0ae13d7c06a7e889c3ab6031bc5d9
Francisco <fmm1005@alu.ubu.es>
2021-04-13 21:02:25 +0200

    Modificación de la clase ActividadSalidaPuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,52 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	//
-	// TODO
-	//
+/**
+ * Clase ActividadSalidaPuerta.
+ *
+ * @author Alvaro L�pez Pereda
+ * @author Francisco Medel Molinero
+ */
+public class ActividadSalidaPuerta implements Runnable{
+
+	/** Constante NUMENTRADAS. */
+	private static final int NUMENTRADAS = 20;
+	
+	/** Puerta. */
+	private String puerta;
+	
+	/** Parque. */
+	private IParque parque;
+
+	/**
+	 * Constructor de la clase.
+	 *
+	 * @param puerta la puerta
+	 * @param parque el parque
+	 */
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	/**
+	 * Run.
+	 */
+	@Override
+	public void run() {
+		for (int i = 0; i < NUMENTRADAS; i ++) {
+			try {
+				parque.salirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}
+	}
 }
